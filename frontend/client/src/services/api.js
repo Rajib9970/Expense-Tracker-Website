023ae0f91ef0ec69a4417db64579f5caa82e5645
Rajib Lochan Dakua <rajiblochandakua5@gmail.com>
2026-06-29 22:49:14 +0530

    Connect frontend to deployed backend

--- frontend/client/src/services/api.js
+++ frontend/client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "http://expense-tracker-website-65kb-8f60zf4yb-rajib05.vercel.app",
 });
 
 export default api;
